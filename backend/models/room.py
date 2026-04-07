10dcccf7ae7faf604b41e7d264a67fa4b20c0d71
유헌상 <gjstkd123@naver.com>
2026-04-07 15:32:58 +0900

    fix#21-import경로수정

--- backend/models/room.py
+++ backend/models/room.py
@@ -1,6 +1,6 @@
 from sqlalchemy import BigInteger, Integer, String, Text, Numeric, TIMESTAMP, text
 from sqlalchemy.orm import Mapped, mapped_column
-from app.db.base import Base
+from db.base import Base
 
 
 class Room(Base):
