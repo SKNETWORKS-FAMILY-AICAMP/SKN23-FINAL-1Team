6e7bf903c5978eee480dcff59c1bec2238007599
rubyheartsping <carnelian12@naver.com>
2026-04-13 11:56:44 +0900

    feat: 서울 원룸 데이터 전처리 모듈 추가

---
+++ market_price_crawling/preprocessing_seoul_oneroom_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("data/seoul_oneroom_data.csv")
+
+numeric_columns = ["buildYear", "dealMonth", "dealYear", "deposit", "excluUseAr", "floor", "monthlyRent"]
+
+for numeric_column in numeric_columns:
+    data[numeric_column] = pd.to_numeric(data[numeric_column], errors = "coerce")
+    data = data.dropna(subset = [numeric_column])
+
+data = data.dropna(subset = ["umdNm"])
+
+conditions = [
+    (data['monthlyRent'] == 0),                                  # 전세
+    (data['deposit'] / data['monthlyRent'] >= 100),                  # 반전세
+    (data['deposit'] / data['monthlyRent'] < 100)                    # 월세
+]
+
+choices = ['전세', '반전세', '월세']
+
+data['class'] = np.select(conditions, choices, default='기타')
+
+del data["semi_basement"]
+del data["wolse"]
+
+data.to_csv("data/seoul_oneroom_data_adjusted.csv", index = False)
