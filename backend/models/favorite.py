d72b04bdbe86922452e25174dd371567a7f0325e
유헌상 <gjstkd123@naver.com>
2026-04-15 14:04:14 +0900

    fix#61-favorite-models수정

--- backend/models/favorite.py
+++ backend/models/favorite.py
@@ -6,15 +6,15 @@ from db.base import Base
 
 class Favorite(Base):
     __tablename__ = "favorite"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('user.user_id', ondelete="CASCADE"), nullable=False)
-    item_id = Column(Integer, ForeignKey("items.item_id", ondelete="CASCADE"), nullable=False)
-    created_at = Column(DateTime, server_default=func.now(), nullable=True)
-
     __table_args__ = (
         UniqueConstraint("user_id", "item_id", name="unique_user_item"),
+        {"schema": "public"},
     )
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("public.user.user_id", ondelete="CASCADE"), nullable=False)
+    item_id = Column(Integer, ForeignKey("public.items.item_id", ondelete="CASCADE"), nullable=False)
+    created_at = Column(DateTime, server_default=func.now(), nullable=True)
 
     user = relationship("User", back_populates="favorites")
     item = relationship("Room", back_populates="favorites")
