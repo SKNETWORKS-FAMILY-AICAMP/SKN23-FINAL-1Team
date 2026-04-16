5a69ad8102c63d2924e7accb776985baa1cf32eb
rubyheartsping <carnelian12@naver.com>
2026-04-16 14:43:55 +0900

    feat: CSV 추출 및 전처리 모듈 작성

--- ml_research/data_processor.py
+++ ml_research/data_processor.py
@@ -12,9 +12,7 @@ def get_csv(filename: str):
     return df
 
 def drop_fake_items(df):
-    print(f"{df["title"].isna().sum()} 개의 가짜 매물 발견.")
-    df = df.dropna(subset = ["title"])
-    print("가짜 매물 처리 완료")
+    df = df.dropna(subset = ["address"])
     return df
 
 def drop_three_room(df):
@@ -49,7 +47,7 @@ def define_class(df):
 
     return df
 
-def integrating_costs(df):
+def converting_monthly_rent(df):
     district_conversion_rate = {
     "종로구": 6.2,
     "중구": 5.8,
@@ -78,7 +76,12 @@ def integrating_costs(df):
     "강동구": 5.1,
     "기타": 5.5,
     }
-    
+    df['conversion_rate'] = df['district'].map(district_conversion_rate)
+    df['converted_monthly_rent'] = (df['deposit'] * df['conversion_rate'] / 12) + df['rent']
+    del df["conversion_rate"]
+    del df["deposit"]
+    del df["rent"]
+    return df
 
 def floor_processing(df):
     df["is_semi_basement"] = df["floor"].str.contains("반지하", na = False)
@@ -86,12 +89,13 @@ def floor_processing(df):
     df['is_first_floor'] = (df['floor'].astype(str).str.strip().isin(['1'])).astype(int)
     df["floor"] = df["floor"].astype(str).str.strip()
     df["all_floors"] = pd.to_numeric(df["all_floors"], errors = "coerce").fillna(1).astype(int)
-    df.loc[df['floor'].str.contains('옥탑방', na=False), 'floor'] = df['all_floors']
-    df.loc[df['floor'].str.contains('반지하', na=False), 'floor'] = -1
-    df.loc[df['floor'].str.contains('저', na=False), 'floor'] = (df['all_floors'] * 0.2).round()
-    df.loc[df['floor'].str.contains('중', na=False), 'floor'] = (df['all_floors'] * 0.5).round()
-    df.loc[df['floor'].str.contains('고', na=False), 'floor'] = (df['all_floors'] * 0.8).round()
+    df.loc[df['floor'].str.contains('옥탑방', na=False), 'floor'] = df['all_floors'].astype(str)
+    df.loc[df['floor'].str.contains('반지하', na=False), 'floor'] = "-1"
+    df.loc[df['floor'].str.contains('저', na=False), 'floor'] = (df['all_floors'] * 0.2).round().astype(int).astype(str)
+    df.loc[df['floor'].str.contains('중', na=False), 'floor'] = (df['all_floors'] * 0.5).round().astype(int).astype(str)
+    df.loc[df['floor'].str.contains('고', na=False), 'floor'] = (df['all_floors'] * 0.8).round().astype(int).astype(str)
     df['floor'] = pd.to_numeric(df['floor'], errors='coerce').astype(int)
+
     df.loc[df['floor'] == 0, 'floor'] = 1
     df["relative_floor"] = df["floor"] / df["all_floors"]
     return df
@@ -101,9 +105,21 @@ def onehot_encoding(df):
     df = pd.get_dummies(df, columns = cols, drop_first = True)
     return df
 
-
-
-
-
 if __name__ == "__main__":
-    pass
+    filename = get_last_csv()
+    print(filename)
+    df = get_csv(filename)
+    processed_df = (
+        df.pipe(drop_fake_items)
+        .pipe(drop_three_room)
+        .pipe(find_gu)
+        .pipe(bathroom_count_jungsanghwa)
+        .pipe(define_class)
+        .pipe(converting_monthly_rent)
+        .pipe(floor_processing)
+        .pipe(onehot_encoding)
+    )
+    processed_df_officetel = processed_df[processed_df["service_type"] == "오피스텔"].drop(columns = ["service_type"])
+    processed_df_oneroom = processed_df[processed_df["service_type"] == "원룸"].drop(columns = ["service_type"])
+    processed_df_officetel.to_csv(f"processed_data/officetel_processed_{filename}", index = False, encoding = "utf-8-sig")
+    processed_df_oneroom.to_csv(f"processed_data/oneroom_processed_{filename}", index = False, encoding = "utf-8-sig")
