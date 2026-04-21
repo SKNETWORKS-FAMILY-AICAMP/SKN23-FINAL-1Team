b5d02552e6c9a1142af63c93f8da75918d73c0c0
유헌상 <gjstkd123@naver.com>
2026-04-21 12:21:56 +0900

    fix#93- config 빌드에러 수정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,25 +2,36 @@ import type { NextConfig } from "next";
 
 const s3Hostname = process.env.NEXT_IMAGE_S3_HOSTNAME;
 
+const remotePatterns: NonNullable<NextConfig["images"]>["remotePatterns"] = [
+  {
+    protocol: "https",
+    hostname: "ic.zigbang.com",
+  },
+  {
+    protocol: "http",
+    hostname: "3.37.97.17",
+    port: "8000",
+    pathname: "/api/images/**",
+  },
+  {
+    protocol: "http",
+    hostname: "3.37.97.17",
+    port: "8000",
+    pathname: "/api/images/**",
+  },
+];
+
+if (s3Hostname) {
+  remotePatterns.push({
+    protocol: "https",
+    hostname: s3Hostname,
+    pathname: "/zigbang_data/images/**",
+  });
+}
+
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "ic.zigbang.com",
-      },
-      {
-        protocol: "https",
-        hostname: s3Hostname || "",
-        pathname: "/zigbang_data/images/**",
-      },
-      {
-        protocol: "http",
-        hostname: "3.37.97.17",
-        port: "8000",
-        pathname: "/api/images/**",
-      },
-    ],
+    remotePatterns,
   },
 };
 
